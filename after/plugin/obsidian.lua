b1d9cee8b1f6f61b0e644e70f907efe4feab6d47
Kevin Christ <christkevin50@gmail.com>
2024-12-02 19:41:07 +0100

    adjusted my obsidian config

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -1,54 +1,53 @@
 require("obsidian").setup({
-  -- Deine komplette Konfiguration hier
-  workspaces = {
-    {
-      name = "personal",
-      path = "~/vaults/personal",
+    -- Deine komplette Konfiguration hier
+    workspaces = {
+        {
+            name = "personal",
+            path = "~/vaults/personal",
+        },
     },
-  },
-  notes_subdir = "notes",
-  daily_notes = {
-    folder = "notes/dailies",
-    date_format = "%Y-%m-%d",
-    alias_format = "%B %-d, %Y",
-    default_tags = { "daily-notes" },
-  },
-  completion = {
-    nvim_cmp = true,
-    min_chars = 2,
-  },
-  mappings = {
-    ["gf"] = {
-      action = function()
-        return require("obsidian").util.gf_passthrough()
-      end,
-      opts = { noremap = false, expr = true, buffer = true },
+    notes_subdir = "notes",
+    daily_notes = {
+        folder = "notes/dailies",
+        date_format = "%Y-%m-%d",
+        alias_format = "%B %-d, %Y",
+        default_tags = { "daily-notes" },
     },
-    ["<leader>ch"] = {
-      action = function()
-        return require("obsidian").util.toggle_checkbox()
-      end,
-      opts = { buffer = true },
+    completion = {
+        nvim_cmp = true,
+        min_chars = 2,
     },
-    ["<cr>"] = {
-      action = function()
-        return require("obsidian").util.smart_action()
-      end,
-      opts = { buffer = true, expr = true },
+    mappings = {
+        ["gf"] = {
+            action = function()
+                return require("obsidian").util.gf_passthrough()
+            end,
+            opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>ch"] = {
+            action = function()
+                return require("obsidian").util.toggle_checkbox()
+            end,
+            opts = { buffer = true },
+        },
+        ["<cr>"] = {
+            action = function()
+                return require("obsidian").util.smart_action()
+            end,
+            opts = { buffer = true, expr = true },
+        },
     },
-  },
-  new_notes_location = "notes_subdir",
-  note_id_func = function(title)
-    local suffix = ""
-    if title ~= nil then
-      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-    else
-      for _ = 1, 4 do
-        suffix = suffix .. string.char(math.random(65, 90))
-      end
-    end
-    return tostring(os.time()) .. "-" .. suffix
-  end,
-  -- Weitere Abschnitte wie in deiner Konfiguration...
+    new_notes_location = "notes_subdir",
+    note_id_func = function(title)
+        local suffix = ""
+        if title ~= nil then
+            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+            for _ = 1, 4 do
+                suffix = suffix .. string.char(math.random(65, 90))
+            end
+        end
+        return tostring(os.time()) .. "-" .. suffix
+    end,
+    -- Weitere Abschnitte wie in deiner Konfiguration...
 })
-
--- lua/whoiskevin/set.lua
+++ lua/whoiskevin/set.lua
@@ -23,4 +23,6 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+vim.opt.conceallevel = 1
+
 vim.g.leader = " "
