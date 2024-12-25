6b98340675001dcb59454840ad44c2c906decf17
Kevin Christ <christkevin50@gmail.com>
2024-12-25 07:25:01 +0100

    changes some things about telescope
    ivy theme and a new keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,21 @@
 return{
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 
+            'nvim-lua/plenary.nvim',
+            {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+        },
         config = function()
+            require('telescope').setup{
+                pickers = {
+                    find_files = {
+                        theme = "ivy"
+                    }
+                }
+            }
             local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = 'Telescope find help_tags' })
             vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
         end,
     }
