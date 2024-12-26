44b042521aabb302bfe56c3844701d1890c98852
Kevin Christ <christkevin50@gmail.com>
2024-12-26 09:17:02 +0100

    changed the colorscheme to be catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,24 @@
 return{
     {
-        "ellisonleao/gruvbox.nvim",
-        lazy = false,
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
         config = function ()
-            vim.cmd.colorscheme("gruvbox")
+            require("catppuccin").setup({
+                flavour = "macchiato",
+                transparent_background = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                }
+            })
+            vim.cmd.colorscheme("catppuccin")
         end,
     },
 }
