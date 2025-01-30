0faf90f445c2f66c8b10a59fee68cad6cd948e9b
Kevin Christ <christkevin50@gmail.com>
2025-01-30 21:21:33 +0100

    cleaned up the config and added some more mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,10 +2,10 @@ return{
     {
         "echasnovski/mini.nvim",
         config = function ()
-            require("mini.statusline").setup({
-                use_icons = true,
-            })
+            require("mini.statusline").setup({})
             require("mini.pairs").setup({})
+            require("mini.notify").setup({})
+            require("mini.icons").setup({})
         end,
     }
 }
